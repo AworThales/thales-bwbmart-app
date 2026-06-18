d2808a73fc6b6b611222ecc647b94cf8819a1734
AworThales <thals4jesus@gmail.com>
2026-06-18 21:20:19 +0100

    Update Amplitude API key

--- src/components/analytics/amplitude.jsx
+++ src/components/analytics/amplitude.jsx
@@ -1,9 +1,11 @@
 import * as amplitude from "@amplitude/analytics-browser";
 
-const API_KEY = "54a139f943ccb7d57016f0bd3cab82d0";
+const API_KEY = "a605fae0b0717fc21b6b13e477f56407";
 
 const init = () => {
-  amplitude.init(API_KEY);
+  amplitude.init(API_KEY, {
+    defaultTracking: true,
+  });
 };
 
 const track = (eventName, properties = {}) => {
